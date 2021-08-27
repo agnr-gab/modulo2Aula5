42212abcbf5a6caf5001462fb61ffc89fdf98aae
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-27 14:10:50 -0300

    Lista de Exercícios 03 - Estrutura de Decisão

---
+++ src/br/com/zup/Modulo02Aula5_27_08_21_ListaExercicios03_ex1.java
@@ -0,0 +1,17 @@
+package br.com.zup;
+
+public class Modulo02Aula5_27_08_21_ListaExercicios03_ex1 {
+
+    public static void main(String[] args) {
+	// Módulo 04 Aula 05 Lista de Exercícios 03 - Estrutura de Decisão
+        // 1 - Faça um programa para o cálculo de uma folha de pagamento, sabendo que os descontos são do Imposto de Renda, que depende do salário bruto (conforme tabela abaixo) e 3% para o Sindicato e que o FGTS corresponde a 11% do Salário Bruto, mas não é descontado (é a empresa que deposita). O Salário Líquido corresponde ao Salário Bruto menos os descontos. O programa deverá pedir ao usuário o valor da sua hora e a quantidade de horas trabalhadas no mês.
+        //Desconto do IR:
+        //Salário Bruto até 900 (inclusive) - isento
+        //Salário Bruto até 1500 (inclusive) - desconto de 5%
+        //Salário Bruto até 2500 (inclusive) - desconto de 10%
+        //Salário Bruto acima de 2500 - desconto de 20%
+        //Imprima na tela as informações, dispostas conforme o exemplo abaixo. No exemplo o valor da hora é 5 e a quantidade de hora é 220.
+
+
+    }
+}
