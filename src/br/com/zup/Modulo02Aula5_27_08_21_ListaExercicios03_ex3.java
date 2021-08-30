b5b0e7a78a07c7399f6ed5ac9a431f80aea34dd1
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-30 10:13:30 -0300

    resolução exercicio 3

--- src/br/com/zup/Modulo02Aula5_27_08_21_ListaExercicios03_ex3.java
+++ src/br/com/zup/Modulo02Aula5_27_08_21_ListaExercicios03_ex3.java
@@ -1,4 +1,30 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Modulo02Aula5_27_08_21_ListaExercicios03_ex3 {
+    public static void main(String[] args) {
+
+        //Enunciado da questão
+        // 3 - Faça um programa que faça 5 perguntas para uma pessoa sobre um crime. As perguntas são:
+        //"Telefonou para a vítima?"
+        //"Esteve no local do crime?"
+        //"Mora perto da vítima?"
+        //"Devia para a vítima?"
+        //"Já trabalhou com a vítima?"
+        //O programa deve no final emitir uma classificação sobre a participação da pessoa no crime. Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como "Assassino". Caso contrário, ele será classificado como "Inocente".
+
+        Scanner leitorInterrogatorio = new Scanner(System.in);
+        int contadorDasPerguntas = 0;
+
+        //nome = nome +1; nome += 1;
+
+        //Perguntas para o usuario
+        String respostaUsuario = leitorInterrogatorio.nextLine();
+
+
+
+
+
+    }
 }
